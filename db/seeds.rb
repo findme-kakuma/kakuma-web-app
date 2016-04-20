57ee2799744c940632ad641e708c239078c23c02
Maxime Burriez <maxime@impala-webstudio.fr>
2016-04-20 15:53:47 +0200

    update import sql with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,42 @@
-[
-  { en: 'South Sudan', sw: 'Sudan Kusini' },
-  { en: 'Sudan',       sw: 'Sudan' },
-  { en: 'Somalia',     sw: 'Somalia' },
-  { en: 'Ethiopia',    sw: 'Ethiopia' },
-  { en: 'D.R. Congo',  sw: 'J.K. Kongo' },
-  { en: 'Burundi',     sw: 'Burundi' },
-  { en: 'Rwanda',      sw: 'Rwanda' },
-  { en: 'Eritrea',     sw: 'Eritrea' },
-  { en: 'Uganda',      sw: 'Uganda' },
-  { en: 'other',       sw: 'mengine' }
-].each do |country|
-  c = Country.new(name: country[:en])
-  c.update_attributes(
-    name: country[:sw],
-    locale: :sw
-  )
+unless Country.any?
+  [
+    { id: 1, en: 'South Sudan', sw: 'Sudan Kusini' },
+    { id: 2, en: 'Sudan',       sw: 'Sudan' },
+    { id: 3, en: 'Somalia',     sw: 'Somalia' },
+    { id: 4, en: 'Ethiopia',    sw: 'Ethiopia' },
+    { id: 5, en: 'D.R. Congo',  sw: 'J.K. Kongo' },
+    { id: 6, en: 'Burundi',     sw: 'Burundi' },
+    { id: 7, en: 'Rwanda',      sw: 'Rwanda' },
+    { id: 8, en: 'Eritrea',     sw: 'Eritrea' },
+    { id: 9, en: 'Uganda',      sw: 'Uganda' },
+    { id: 10, en: 'other',      sw: 'mengine' }
+  ].each do |country|
+    c = Country.new(
+      id: country[:id],
+      name: country[:en]
+    )
+    c.update_attributes(
+      name: country[:sw],
+      locale: :sw
+    )
+  end
+end
+
+unless Resident.any?
+  require 'csv'
+
+  csv_text = File.read('tmp/residents.csv')
+  csv = CSV.parse(csv_text, headers: true)
+  csv.each do |row|
+    h = row.to_hash
+    h['country_id'] = 10 if h['country_id'] == 'NULL'
+    h['place'] = nil if h['place'] == 'NULL'
+    begin
+      r = Resident.new(h)
+      r.save validate: false
+    rescue Exception => e
+      puts e.message
+      puts "row: #{h}"
+    end
+  end
 end
